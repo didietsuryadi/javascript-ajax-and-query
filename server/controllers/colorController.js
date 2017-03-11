89f72ef41e3a92517d2cd9d8ac24758c0b96b0e9
didietsuryadi <didietsuryadi>
2017-03-12 04:44:32 +0700

    inisialisasi-folder

---
+++ server/controllers/colorController.js
@@ -0,0 +1,9 @@
+module.exports = {
+  changeColor: function(req, res, next) {
+    let randomPosition = Math.floor(Math.random() * 9)
+    let randomRgb1 = Math.floor(Math.random() * 255)
+    let randomRgb2 = Math.floor(Math.random() * 255)
+    let randomRgb3 = Math.floor(Math.random() * 255)
+    res.json({i: randomPosition, rgb1: randomRgb1, rgb2: randomRgb2, rgb3: randomRgb3})
+  }
+}
